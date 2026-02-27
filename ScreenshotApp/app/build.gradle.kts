16d8b60dc5d4edd409adf1acb4d207600ad644a1
User662122 <gwkrishna06@gmail.com>
2026-02-27 11:42:17 +0530

    Update build.gradle.kts

--- ScreenshotApp/app/build.gradle.kts
+++ ScreenshotApp/app/build.gradle.kts
@@ -37,20 +37,19 @@ android {
     buildFeatures {
         viewBinding = true
     }
-
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    
-    
-    
+
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+    // ✅ OKHTTP (IMPORTANT)
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
-    
-    
