f68c4548a09786507b21dc660f76e57d48f95355
User662122 <gwkrishna06@gmail.com>
2026-03-01 23:35:51 +0530

    Update build.gradle.kts

--- ScreenshotApp/build.gradle.kts
+++ ScreenshotApp/build.gradle.kts
@@ -1,8 +1,16 @@
-plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+// Top-level build file
+buildscript {
+    ext.kotlin_version = "1.9.22"
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath 'com.android.tools.build:gradle:8.2.2'
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+    }
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+task clean(type: Delete) {
+    delete rootProject.buildDir
 }
