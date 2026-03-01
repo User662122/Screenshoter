1a8e2b6f0b6ccacdd39ee728a6b746b8c561b537
User662122 <gwkrishna06@gmail.com>
2026-03-01 23:34:19 +0530

    Update build.gradle.kts

--- ScreenshotApp/app/build.gradle.kts
+++ ScreenshotApp/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id 'com.android.application'
+    id 'kotlin-android'
 }
 
 android {
@@ -25,9 +26,14 @@ android {
         sourceCompatibility JavaVersion.VERSION_1_8
         targetCompatibility JavaVersion.VERSION_1_8
     }
+
+    kotlinOptions {
+        jvmTarget = '1.8'
+    }
 }
 
 dependencies {
     implementation 'androidx.appcompat:appcompat:1.6.1'
     implementation 'com.google.android.material:material:1.11.0'
+    implementation "org.jetbrains.kotlin:kotlin-stdlib:1.9.22"
 }
